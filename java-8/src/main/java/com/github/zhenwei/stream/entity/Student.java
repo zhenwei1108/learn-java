6deb7d969a86f1ca5988dca1ff4794b9fa6005ab
zhenwei1108 <zhenwei1108@163.com>
2023-07-03 13:58:39 +0800

    commit obj

---
+++ java-8/src/main/java/com/github/zhenwei/stream/entity/Student.java
@@ -0,0 +1,20 @@
+package com.github.zhenwei.stream.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author: zhangzhenwei
+ * @description:
+ * @date: 2023/7/3  13:46
+ * @since: 1.0.0
+ */
+@Data
+@AllArgsConstructor
+public class Student {
+
+  private String name;
+
+  private int age;
+
+}
